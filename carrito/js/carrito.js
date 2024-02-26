7816d72e46f153594f79b008a092c386f1d92dd3
Flash-sonic-98 <151184031+Flash-sonic-98@users.noreply.github.com>
2024-02-25 20:28:10 -0500

    Add files via upload

---
+++ carrito/js/carrito.js
@@ -0,0 +1,16 @@
+var contenedor = document.querySelector(".contenedor")
+var llantacas1 = document.querySelector(".llantacas1")
+var llantacas2 = document.querySelector(".llantacas2")
+var humo =document.querySelector(".humo")
+var parpadeo =document.querySelector(".parpadeo")
+
+contenedor.addEventListener('click', fondiu)
+
+function fondiu(){
+    contenedor.classList.add('fondomover')
+    llantacas1.classList.add('llantamover')
+    llantacas2.classList.add('llantamover')
+    humo.classList.add('humomover')
+    parpadeo.classList.add('parpadeopare')
+
+}
